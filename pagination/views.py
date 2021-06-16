493784fe6a4a1fee70b7e4e7f3ae002262f7f34f
NigaraOmarova <nigara909095@gmail.com>
2021-06-16 18:23:36 +0600

    test

---
+++ pagination/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+
+from django.shortcuts import render
+from rest_framework import generics, permissions
+from rest_framework.pagination import PageNumberPagination
+
+from main import serializers
+from main.models import Article
+
+
+class StandardResultPagination(PageNumberPagination):
+    page_size = 100
+    page_query_param = 'page_size'
+    max_page_size = 1000
+
+
+class ProductListView(generics.ListAPIView):
+    queryset = Article.objects.all()
+    serializer_class = serializers.ProductSerializer
+    permission_class = (permissions.IsAuthenticated, )
+    pagination_class = StandardResultPagination
+
